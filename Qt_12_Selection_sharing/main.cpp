240a71a554104612374ae603ae71ca17ecb426a3
hydrargyyrum <mrs.valerya@yandex.ru>
2022-12-31 01:16:50 +0300

    First

---
+++ Qt_12_Selection_sharing/main.cpp
@@ -0,0 +1,36 @@
+#include <QtWidgets>
+
+ // ----------------------------------------------------------------------
+ int main(int argc, char** argv)
+ {
+     QApplication app(argc, argv);
+     QWidget      wgt;
+
+     QStringListModel model;
+     model.setStringList(QStringList() << "Item1" << "Item2" << "Item3");
+
+     QTreeView* pTreeView = new QTreeView;
+     pTreeView->setModel(&model);
+
+     QListView* pListView = new QListView;
+     pListView->setModel(&model);
+
+     QTableView* pTableView = new QTableView;
+     pTableView->setModel(&model);
+
+    QItemSelectionModel selection(&model);
+    pTreeView->setSelectionModel(&selection);
+    // pListView->setSelectionModel(&selection);
+     pTableView->setSelectionModel(&selection);
+
+     //Layout setup
+     QHBoxLayout* phbxLayout = new QHBoxLayout;
+     phbxLayout->addWidget(pTreeView);
+     phbxLayout->addWidget(pListView);
+     phbxLayout->addWidget(pTableView);
+     wgt.setLayout(phbxLayout);
+
+     wgt.show();
+
+     return app.exec();
+ }
---
+++ Qt_13_WidgetPalette/main.cpp
@@ -0,0 +1,27 @@
+#include <QtWidgets>
+
+ // ----------------------------------------------------------------------
+ int main(int argc, char** argv)
+ {
+     QApplication app(argc, argv);
+     QSpinBox     spb;
+
+     QPalette pal(Qt::red, Qt::green);
+     QApplication::setPalette(pal);
+
+    /* QPalette pal = spb.palette();
+     pal.setBrush(QPalette::Button, QBrush(Qt::red, Qt::Dense3Pattern));
+     pal.setColor(QPalette::ButtonText, Qt::blue);
+     pal.setColor(QPalette::Text, Qt::magenta);
+     pal.setColor(QPalette::Active, QPalette::Base, Qt::green);
+
+     spb.setPalette(pal);
+    */
+
+     spb.resize(150, 74);
+     spb.show();
+
+     app.setStyle("Windows");
+
+     return app.exec();
+ }
